571235cf5cb7f2df28d7f1e318b3da49027bc352
SashaCollins <anja.wimmer@hotmail.de>
2020-05-25 19:44:16 +0200

    demo on frontend

---
+++ visualization/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <test></test>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import test from './components/test.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    test
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
