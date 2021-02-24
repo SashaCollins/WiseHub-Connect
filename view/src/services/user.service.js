a813220d77f9ed690415d442e162ee4da1e042ab
SashaCollins <wimmer18@hm.edu>
2021-02-24 18:18:50 +0100

    Update user.service.js

--- view/src/services/user.service.js
+++ view/src/services/user.service.js
@@ -13,43 +13,48 @@ class UserService {
   fetchProfile(user) {
    return axios.post(API_USER_URL + 'profile', {
      email: user.email
-   }
+   }, { headers: authHeader() }
   ).then((response) => {
      return response;
    });
   }
+  
   updateEmail(payload) {
     return axios.post(API_USER_URL + 'update/email', {
       option: 'email',
       email: payload.oldEmail,
       new_email: payload.newEmail
-    })
+    }, { headers: authHeader() })
   }
+  
   updateCredentials(payload) {
     return axios.post(API_USER_URL + 'update/credentials', {
       option: 'credentials',
       email: payload.email,
       plugins: payload.plugins
-    })
+    }, { headers: authHeader() })
   }
+  
   updatePassword(user) {
     //let hashedPassword = require('crypto').createHash('sha512').update(user.password).digest('hex');
     return axios.post(API_USER_URL + 'update/password', {
       option: 'password',
       email: user.email,
       password: sha3(user.password).toString()
-    })
+    }, { headers: authHeader() })
   }
+  
   deleteAccount(user) {
     return axios.post(API_USER_URL + 'delete', {
       email: user.email
-    })
+    }, { headers: authHeader() })
   }
+  
   fetchData(payload) {
     return axios.post(API_DATA_URL + "all", {
           option: payload.option,
           email: payload.user.email
-        }
+        }, { headers: authHeader() }
     ).then((response) => {
       return response;
     });
