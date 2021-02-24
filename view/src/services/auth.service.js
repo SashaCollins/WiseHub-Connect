c52897748654679b0de63625bd2a50375877f373
SashaCollins <wimmer18@hm.edu>
2021-02-24 18:10:25 +0100

    Update auth.service.js

--- view/src/services/auth.service.js
+++ view/src/services/auth.service.js
@@ -1,22 +1,28 @@
 import axios from 'axios';
+import authHeader from "./auth-header";
+import { secure } from './encryption.service';
+import sha3 from 'crypto-js/sha3';
+
 
 const API_URL = '/api/auth/';
 
+
 class AuthService {
     login(user) {
-        let hashedPassword = require('crypto').createHash('sha512')
-            .update(user.password).digest('hex');
+        //let hashedPassword = require('crypto').createHash('sha512').update(user.password).digest('hex');
         return axios.post(API_URL + 'signin', {
             email: user.email,
-            password: hashedPassword
+            password: sha3(user.password).toString()
         }).then((response) => {
             //set user to loggedIn
             if (response.data) {
                 if (response.data.success) {
                     user.password = "";
-                    sessionStorage.setItem('loggedIn', response.data.success);
+                    secure.set('user', JSON.stringify(user));
+                    secure.set('loggedIn', response.data.success);
+                    //sessionStorage.setItem('loggedIn', response.data.success);
                     // browser session storage for user module
-                    sessionStorage.setItem('user', JSON.stringify(user));
+                    //sessionStorage.setItem('user', JSON.stringify(user));
                 }
             }
             return response;
@@ -24,19 +30,23 @@ class AuthService {
     }
 
     logout() {
+        secure.removeAll();
         sessionStorage.clear();
         localStorage.clear();
     }
 
     register(user) {
-        let hashedPassword = require('crypto').createHash('sha512')
-            .update(user.password).digest('hex');
+        //let hashedPassword = require('crypto').createHash('sha512').update(user.password).digest('hex');
         return axios.post(API_URL + 'signup', {
             name: user.name,
-            password: hashedPassword,
+            password: sha3(user.password).toString(),
             email: user.email
         },
         );
+    }
+    
+    validate_token(token) {
+        return axios.get(API_URL + 'validate/' + token);
     }
 }
 
